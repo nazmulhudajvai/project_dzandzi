9bf9e70fbcbf643fee5027c9212c3fe268df39bd
Pronoy <pronoysarkar1020@gmail.com>
2025-10-28 04:58:20 +0600

    add color file

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tinny_app/presentation/pages/auth/login/login_screen.dart';
-import 'package:tinny_app/presentation/pages/common/notify.dart';
 import 'package:tinny_app/presentation/pages/auth/sign_up/sign_up_screen.dart';
 import 'package:tinny_app/presentation/pages/auth/otp/otp_verify_view.dart';
 import 'package:tinny_app/presentation/pages/splash/splash_view.dart';
@@ -19,7 +18,6 @@ class AppPages {
     // Splash View Route
     GetPage(name: _Paths.SplashView, page: () => SplashView()),
     // Login Route
-    GetPage(name: _Paths.Notification, page: () => CustomNotificationPage()),
     GetPage(name: _Paths.LOGIN, page: () => LoginScreen()),
     // Sign Up Route
     GetPage(name: _Paths.SIGNUP, page: () => SignUpView()),
